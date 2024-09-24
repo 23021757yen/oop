a60d5ad654cd53af839e2f611a35e67fd36cbe50
unknown <23021757@vnu.edu.vn>
2024-09-24 22:46:46 +0700

    first commit

---
+++ HW1/Introduction/src/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Thi Ngoc Yen \t 23021757 \t K68I-CS3 \t 23021757yen \t 23021757@vnu.edu.vn");
+        for(int i=9;i>+1;i--)
+        {
+            System.out.println(i + " bottle s of beer on the wall," + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
