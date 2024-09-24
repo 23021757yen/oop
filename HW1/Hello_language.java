466b945408d1dddb601024f215fe6cee87d8e47f
unknown <23021757@vnu.edu.vn>
2024-09-24 23:22:41 +0700

    first commit

---
+++ HW1/Hello_language.java
@@ -0,0 +1,8 @@
+public class Hello_language {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Xin chao");
+        System.out.println("Ciao");
+
+    }
+}
