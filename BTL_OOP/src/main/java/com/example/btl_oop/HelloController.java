222a154be7f6370af700f34f4066027a06902f1b
unknown <23021757@vnu.edu.vn>
2024-10-10 19:36:31 +0700

    second commit

---
+++ BTL_OOP/src/main/java/com/example/btl_oop/HelloController.java
@@ -0,0 +1,8 @@
+package com.example.btl_oop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+}
---
+++ BTL_OOP/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.btl_oop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.btl_oop to javafx.fxml;
+    exports com.example.btl_oop;
+}
